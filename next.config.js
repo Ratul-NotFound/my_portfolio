f4615f79a40a6064d50455871528fcadb603fb48
Ratul-DIU <m.h.ratul18@gmail.com>
2026-05-20 02:30:41 +0600

    Configure next.config.js for static HTML export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   compress: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
